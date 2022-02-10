7ca9a04bc5ee1e3d2b6e25632868ea5a0b8210db
tfbkev <86990677+tfbkev@users.noreply.github.com>
2022-02-10 12:19:56 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // final ArrayList<Automobile> vehicleList = new ArrayList<>();
 
-        Scanner scnr = new Scanner(System.in);
+        // Scanner scnr = new Scanner(System.in);
+        // System.out.println();
+        // String cartestName = scnr.nextLine();
+        // int indexofTestyear = vehicleList.indexOf(cartestName);
+        // if (indexofTestyear != -1) {
+        //     System.out.println("Add a Vehicle");
+        //     System.out.println("Enter Vehicle Make: ");
+        //     String make = scnr.next();
+        //     System.out.println("Enter Vehicle Model: ");
+        //     String model = scnr.next();
+        //     System.out.println("Enter Vehicle Color: ");
+        //     String color = scnr.next();
+        //     System.out.println("Enter Vehicle Year: ");
+        //     int year = scnr.nextInt();
+        //     System.out.println("Enter Vehicle Mileage: ");
+        //     int mileage = scnr.nextInt();
+
+        //     Automobile newAuto = new Automobile(make, model, color, year, mileage);
+        //     vehicleList.add(newAuto);
+        //     for (int i = 0; i < (vehicleList).size(); i++) {
+        //     }
+        // }
+
+        
+        
+        
+        
+        
+        
+        
+        // Scanner scnr = new Scanner(System.in);
         // Create object using automobile construcot
         // Automobile car1 = new Automobile("BMW", "X1", "RED", 2021, 1400);
         // final ArrayList<Automobile> carList = new ArrayList<>();
